162384bff7a3103f8b4a1a4c6802389089ed4d22
dhinforto <geraldineforto@gmail.com>
2016-08-30 10:52:37 +0800

    Initial Project

---
+++ BallClub/BallClub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BallClub
+//
+//  Created by Geraldine Forto on 30/08/2016.
+//  Copyright © 2016 Geraldine Forto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
