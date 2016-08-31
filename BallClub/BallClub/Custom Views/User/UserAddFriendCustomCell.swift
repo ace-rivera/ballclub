a43ce028544d06c9d5a3e0deed7c1a745c292bda
dhinforto <geraldineforto@gmail.com>
2016-08-31 15:30:26 +0800

    XIBs

---
+++ BallClub/BallClub/Custom Views/User/UserAddFriendCustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserAddFriendCustomCell.swift
+//  BallClub
+//
+//  Created by Geraldine Forto on 31/08/2016.
+//  Copyright © 2016 Geraldine Forto. All rights reserved.
+//
+
+import UIKit
+
+class UserAddFriendCustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
