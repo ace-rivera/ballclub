9630b9345853a6722cb6c758b3f4c3103001e0dc
Geraldine Forto <geraldineforto@gmail.com>
2016-09-06 23:40:31 +0800

    Facebook Login

--- BallClub/BallClub/Controllers/Login/LoginViewController.swift
+++ BallClub/BallClub/Controllers/Login/LoginViewController.swift
@@ -7,46 +7,77 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
-    
-    @IBOutlet weak var forgotDetailsButton: UIButton!
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var passwordLabel: UITextField!
-    @IBOutlet weak var emailAddLabel: UITextField!
-    @IBOutlet weak var backgroundImage: UIImageView!
-    @IBOutlet weak var appLogo: UIImageView!
+  
+  @IBOutlet weak var forgotDetailsButton: UIButton!
+  @IBOutlet weak var loginButton: FBSDKLoginButton!
+  @IBOutlet weak var passwordLabel: UITextField!
+  @IBOutlet weak var emailAddLabel: UITextField!
+  @IBOutlet weak var backgroundImage: UIImageView!
+  @IBOutlet weak var appLogo: UIImageView!
   
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewWillAppear(animated: Bool) {
-        setupUI()
-    }
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    setupUI()
+  }
+  
+  func setupUI() {
+    self.navigationController?.navigationBar.hidden = true
+    loginButton.layer.borderColor = UIColor.lightGrayColor().CGColor
+    loginButton.layer.borderWidth = 1
     
-    func setupUI() {
-        self.navigationController?.navigationBar.hidden = true
-        loginButton.layer.borderColor = UIColor.lightGrayColor().CGColor
-        loginButton.layer.borderWidth = 1
-        
-        emailAddLabel.attributedPlaceholder = NSAttributedString(string:"E-mail",
-                                                                 attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
-        passwordLabel.attributedPlaceholder = NSAttributedString(string:"Password",
-                                                                 attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
-        
-        
-    }
+    emailAddLabel.attributedPlaceholder = NSAttributedString(string:"E-mail",
+                                                             attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
+    passwordLabel.attributedPlaceholder = NSAttributedString(string:"Password",
+                                                             attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-    @IBAction func signUpHereButtonPressed(sender: AnyObject) {
-        self.performSegueWithIdentifier("LoginToSignUpSegue", sender: self)
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  //MARK: - IBAction
+  @IBAction func loginButtonPressed(sender: AnyObject) {
+    self.performSegueWithIdentifier("LoginToMainSegue", sender: self)
+  }
+  
+  @IBAction func loginWithFacebookButtonPressed(sender: AnyObject) {
+
+    let login = FBSDKLoginManager()
+    login.logInWithReadPermissions(["public_profile","email"], fromViewController: self) { (result, error) in
+      if ((error) != nil)  {
+        NSLog("Facebook Process error: %@", error);
+      }
+      else if (result.isCancelled)
+      {
+        NSLog("Facebook Cancelled");
+      }
+      else {
+        if ((FBSDKAccessToken.currentAccessToken()) != nil) {
+          FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, email, gender"]).startWithCompletionHandler { (connection, result, error) in
+            if error == nil {
+              print("Facebook Fetched user : \(result)")
+              //TODO: Save user details
+              self.performSegueWithIdentifier("LoginToMainSegue", sender: self)
+            }
+          }
+        }
+      }
     }
-    
+  }
+  
+  @IBAction func signUpHereButtonPressed(sender: AnyObject) {
+    self.performSegueWithIdentifier("LoginToSignUpSegue", sender: self)
+  }
+  
 }
--- BallClub/BallClub/Controllers/Login/SignUpViewController.swift
+++ BallClub/BallClub/Controllers/Login/SignUpViewController.swift
@@ -10,62 +10,65 @@ import UIKit
 import MaterialKit
 
 class SignUpViewController: UIViewController {
+  
+  @IBOutlet weak var createProfileButton: UIButton!
+  @IBOutlet weak var passwordTextField: MKTextField!
+  @IBOutlet weak var confirmPasswordTextField: MKTextField!
+  @IBOutlet weak var emailTextField: MKTextField!
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBOutlet weak var createProfileButton: UIButton!
+    setupUI()
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func setupUI(){
     
-    @IBOutlet weak var passwordTextField: MKTextField!
-    @IBOutlet weak var confirmPasswordTextField: MKTextField!
-    @IBOutlet weak var emailTextField: MKTextField!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setupUI()
-        // Do any additional setup after loading the view.
-    }
+    self.navigationController?.navigationBar.hidden = false
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    var image = UIImage(named: "back")
     
-    func setupUI(){
-        
-        self.navigationController?.navigationBar.hidden = false
-        
-        var image = UIImage(named: "back")
-        
-        image = image?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
-        
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: UIBarButtonItemStyle.Plain, target: self, action: #selector(SignUpViewController.backButtonPressed))
-        
-        
-        createProfileButton.layer.borderColor = UIColor.lightGrayColor().CGColor
-        createProfileButton.layer.borderWidth = 1
-      
-        emailTextField.attributedPlaceholder = NSAttributedString(string:"E-mail",
-                                                                  attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
-        passwordTextField.attributedPlaceholder = NSAttributedString(string:"Password",
-                                                                     attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
-        confirmPasswordTextField.attributedPlaceholder = NSAttributedString(string:"Confirm Password",
-                                                                            attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
-        
-        
-        emailTextField.layer.borderColor = UIColor.clearColor().CGColor
-        passwordTextField.layer.borderColor = UIColor.clearColor().CGColor
-        confirmPasswordTextField.layer.borderColor = UIColor.clearColor().CGColor
-        
-        
-        
-    }
+    image = image?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
     
-    func backButtonPressed(){
-        self.navigationController?.popViewControllerAnimated(true)
-        
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        self.view.endEditing(true)
-    }
+    self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: UIBarButtonItemStyle.Plain, target: self, action: #selector(SignUpViewController.backButtonPressed))
     
     
+    createProfileButton.layer.borderColor = UIColor.lightGrayColor().CGColor
+    createProfileButton.layer.borderWidth = 1
+    
+    emailTextField.attributedPlaceholder = NSAttributedString(string:"E-mail",
+                                                              attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
+    passwordTextField.attributedPlaceholder = NSAttributedString(string:"Password",
+                                                                 attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
+    confirmPasswordTextField.attributedPlaceholder = NSAttributedString(string:"Confirm Password",
+                                                                        attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
+    
+    
+    emailTextField.layer.borderColor = UIColor.clearColor().CGColor
+    passwordTextField.layer.borderColor = UIColor.clearColor().CGColor
+    confirmPasswordTextField.layer.borderColor = UIColor.clearColor().CGColor
+    
+    
+    
+  }
+  
+  func backButtonPressed(){
+    self.navigationController?.popViewControllerAnimated(true)
+    
+  }
+  
+  override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    self.view.endEditing(true)
+  }
+  
+  //MARK: - IBAction
+  @IBAction func createProfileButtonPressed(sender: AnyObject) {
+    self.performSegueWithIdentifier("SignUpToCreateProfileSgue", sender: self)
+  }
+  
 }
