5b7c86d014a3031b906228e380ad68f41313779f
Geraldine Forto <geraldineforto@gmail.com>
2016-09-08 00:42:37 +0800

    Game Detail

--- BallClub/BallClub/Controllers/Games/GameDetailViewController.swift
+++ BallClub/BallClub/Controllers/Games/GameDetailViewController.swift
@@ -8,15 +8,35 @@
 
 import UIKit
 
-class GameDetailViewController: UIViewController {
+class GameDetailViewController: UITableViewController {
+  
+  @IBOutlet var gameDetailTableView: UITableView!
+  
+  @IBOutlet weak var locationImage: UIImageView!
+  @IBOutlet weak var gameDate: UILabel!
+  @IBOutlet weak var gameTitle: UILabel!
+  @IBOutlet weak var gameLocation: UILabel!
+  @IBOutlet weak var gamePrice: UILabel!
+  @IBOutlet weak var gameTime: UILabel!
+  @IBOutlet weak var gameDetails: UILabel!
+  @IBOutlet weak var playerCount: UILabel!
+  @IBOutlet weak var playerNames: UILabel!
+  @IBOutlet weak var playerCollection: UICollectionView!
+  @IBOutlet weak var additionInfo: UILabel!
   
   //MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    setUpUI()
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
+  }
+  
+  //MARK: - SetUpUI
+  func setUpUI(){
+    self.gameDetailTableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: self.gameDetailTableView.bounds.size.width, height: 0.01)) //remove header - extra space above tableview
   }
   
   //MARK: - IBAction
@@ -27,5 +47,20 @@ class GameDetailViewController: UIViewController {
   @IBAction func seeAllButtonPressed(sender: AnyObject) {
     self.performSegueWithIdentifier("DetailToInvitedSegue", sender: self)
   }
-
+  
+  @IBAction func notGoingPressed(sender: AnyObject) {
+    
+  }
+  
+  @IBAction func tentativeButtonPressed(sender: AnyObject) {
+    
+  }
+  
+  @IBAction func goingButtonPressed(sender: AnyObject) {
+    
+  }
+  
+  @IBAction func suggestInviteButtonPressed(sender: AnyObject) {
+    
+  }
 }
