72c670e13d28ec61cea47d71e3f684132b3c7912
Joshua Relova <joshuajohn.relova@gmail.com>
2017-01-08 15:12:13 +0800

    Get, Create and Delete Requests API calls implementation 100%

---
+++ BallClub/BallClub/Request.swift
@@ -0,0 +1,13 @@
+//
+//  Request.swift
+//  BallClub
+//
+//  Created by Joshua Relova on 1/8/17.
+//  Copyright © 2017 Geraldine Forto. All rights reserved.
+//
+
+import UIKit
+
+class Request: NSObject {
+
+}
